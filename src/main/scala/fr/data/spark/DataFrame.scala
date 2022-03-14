117485e9ae374175b90598797cc6e5757da5724a
juliengan <julie.ngan68@gmail.com>
2022-03-15 00:29:36 +0100

    rectifications

--- src/main/scala/fr/data/spark/DataFrame.scala
+++ src/main/scala/fr/data/spark/DataFrame.scala
@@ -1,11 +1,14 @@
 package fr.data.spark
 
+import org.apache.hadoop.shaded.com.ctc.wstx.util.DataUtil.Integer
+import org.apache.hadoop.shaded.javax.xml.bind.DatatypeConverter.{parseInt, parseString}
 import org.apache.spark
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.catalyst.dsl.expressions.{DslAttr, StringToAttributeConversionHelper}
 import org.apache.spark.sql.catalyst.expressions.Literal
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.IntegerType
 
 object DataFrame {
   val pathToFile = "data/codesPostaux.csv"
@@ -24,15 +27,29 @@ object DataFrame {
 
 
   def main(args: Array[String]): Unit = {
-    println(loadData().printSchema())
     val df = loadData()
-    println(df.select("Nom_commune").count())
-    println(df.select("Nom_commune").filter("Ligne_5 is not null").count())
-    val new_df = df.withColumn("departement", (col("Code_commune_INSEE")/ 100))
-    val ordered = new_df.orderBy("Code_postal")
-    ordered.write.format("csv").save("commune_et_departement.csv")
-    ordered.filter(col("departement") === "02").map {x => println(col("Nom_commune")) ; x}
 
-    println(ordered.select("departement").filter("Nom_commune.count() => 10")
+    // 1 ) Schema of the dataframe
+    println(df.printSchema())
+
+    // 2 ) Number of municipalities
+    println(df.select("Nom_commune").count())
+
+    // 3 ) Number of municipalities having non-null Ligne_5 attribute
+    println(df.select("Nom_commune").filter("Ligne_5 is not null").count())
+
+    // 4 ) Add department column
+    val new_dff = df.withColumn("departement", (col("Code_commune_INSEE") / 1000).cast("integer"))
+
+    // 5 ) Save ordered dataframe by postal code as a csv
+    // val ordered = new_df.orderBy("Code_postal")
+    //ordered.write.format("csv").save("commune_et_departement.csv")
+
+    // 6 ) Display municipalities of Aisne department
+    val aisne = new_dff.select("Nom_commune").filter("departement = 2")
+      aisne.foreach(x => println(x))
+
+    // 7 ) Display department with the most municipalities
+    new_dff.groupBy(new_dff("Nom_commune")).agg(count("*").as("columnCount")).orderBy("columnCount").show(1)
   }
 }
